4a15f717237232e4bcdde4accf335ce7bc9d13f0
up1 <somkiat.p@gmail.com>
2023-08-16 00:06:55 +0700

    Working with API

--- web-react/src/App.tsx
+++ web-react/src/App.tsx
@@ -1,32 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const client = axios.create({
+  baseURL: "/api" 
+});
+
+const App = () => {
+  const [message, setMessage] = useState('');
+
+   // GET with Axios
+  useEffect(() => {
+     const fetchAPI = async () => {
+        let response = await client.get('/hi');
+        setMessage(response.data.message);
+     };
+     fetchAPI();
+  });
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React 3333</h1>
+      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <p id="hello_message">
+          {message}
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
